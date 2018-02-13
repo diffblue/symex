41693edbc166e522e2235ccae82b621455f6ccb8
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-02-13 19:17:11 +0000

    new API for show_goto_functions

--- src/symex/symex_parse_options.cpp
+++ src/symex/symex_parse_options.cpp
@@ -375,7 +375,7 @@ bool symex_parse_optionst::process_goto_program(const optionst &options)
     // show it?
     if(cmdline.isset("show-goto-functions"))
     {
-      show_goto_functions(goto_model, get_ui());
+      show_goto_functions(goto_model, get_message_handler(), get_ui());
       return true;
     }
   }
