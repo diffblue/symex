5ae0ebb641b35f600530ddf993ec132f9235f4b9
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-11-30 20:52:53 -0500

    added --compact-trace

--- src/symex/symex_parse_options.cpp
+++ src/symex/symex_parse_options.cpp
@@ -466,6 +466,7 @@ void symex_parse_optionst::report_properties(
 
     if((cmdline.isset("show-trace") ||
         cmdline.isset("trace") ||
+        cmdline.isset("compact-trace") ||
         cmdline.isset("stack-trace") ||
         cmdline.isset("xml-ui") ||
         cmdline.isset("stop-on-fail")) &&
